384a04cb568f4b8c1b5d762f6fcedf3245f72483
lindsayka9 <kat.lindsay.92@gmail.com>
2018-02-02 13:06:15 -0500

    Add GET events
    Add GET book events in `events.js`
    Add event handlers
    Require event handlers in `index.js`

---
+++ assets/scripts/events.js
@@ -0,0 +1,25 @@
+'use strict'
+const api = require('./api')
+const ui = require('./ui')
+
+const onGetBook = function (event) {
+  event.preventDefault()
+  api.getBook()
+    .then(ui.showBooks)
+    .catch(console.log('error'))
+}
+const onGetBooks = function (event) {
+  event.preventDefault()
+  api.getBooks()
+    .then(ui.showBooks)
+    .catch(console.log('error'))
+}
+
+const addHandlers = function (data) {
+  $('#get-book').on('submit', onGetBook)
+  $('#all-books-button').on('click', onGetBooks)
+}
+
+module.exports = {
+  addHandlers
+}
